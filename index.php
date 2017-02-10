c38c7ebdd5e62191fec9d0a7bca9c1b0fc91688d
Don Slaughter <allenslaughter@me.com>
2017-02-10 14:20:01 -0500

    Initial Upload

---
+++ index.php
@@ -0,0 +1,462 @@
+<DOCTYPE! html>
+<head>
+	<title>Elite Barber Shop</title>
+	  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+  <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="barberStyle.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+</head>
+<body>
+<!--Name-->
+<a name="home"></a>
+		<div class="row">
+			<div class="col-12">
+				<div id="nav">
+					<ul>
+						<li><a href="#home">Home <span class="fa fa-home"></span></a></li>
+						<li><a href="#serv">Services <span class="fa fa-list-alt"></span></a></li>
+						<li><a href="#loc">Location <span class="fa fa-map-marker"></span></a></li>
+						<li><a href="#con">Contact <span class="fa fa-phone"></span></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+
+<div class="row">
+	<div class="col-12">
+		<section class="paraMain">
+			<div class="paraInnerMain">
+				<center><img src="Images/Elite Logo1.png" class="logo" alt="Elite Barber Shop Logo"></center>
+			</div>
+		</section>
+	</div>
+</div>
+
+<div class="row">
+	<br />
+	<div class="col-12">
+		<center>
+		<div class="cardL">
+			<div class="cardH">
+				<h3>Why the Name Elite?</h3>
+			</div>
+			<div class="subTXT">
+				<p>Elite Barber Shop provides a comfortable environment to relax, listen to light music or watch sports while getting your haircut/service. I Know your time is valuable so schedule an appointment and cut out the wait time.</p>
+			</div>
+		</div>
+		</center>
+	</div>
+</div>
+<!--Reviews-->
+<br />
+<div class="row">
+<div id="revRow">
+	<div class="col-3">
+		<center>
+		<div class="cardRev">
+			<div class="cardH">
+				<div class="star"><span class="fa fa-star"></span> <span class="fa fa-star"></span> <span class="fa fa-star"></span> <span class="fa fa-star"></span> <span class="fa fa-star"></span></div>
+			</div>
+			<div class="subTXT">
+				<p>"Best barber in Georgetown!"</p>
+				<p class="name"><strong>- Dewayne Hancock</strong></p>
+			</div>
+		</div>
+	</center>
+	</div>
+
+	<div class="col-3">
+		<center>
+		<div class="cardRev">
+			<div class="cardH">
+				<div class="star"><span class="fa fa-star"></span> <span class="fa fa-star"></span> <span class="fa fa-star"></span> <span class="fa fa-star"></span> <span class="fa fa-star"></span></div>
+			</div>
+			<div class="subTXT">
+				<p>"Best barber I've seen in a long time."</p>
+				<p class="name"><strong>- Rory Allison</strong></p>
+			</div>
+		</div>
+		</center>
+	</div>
+
+	<div class="col-3">
+		<center>
+		<div class="cardRev">
+			<div class="cardH">
+				<div class="star"><span class="fa fa-star"></span> <span class="fa fa-star"></span> <span class="fa fa-star"></span> <span class="fa fa-star"></span> <span class="fa fa-star"></span></div>
+			</div>
+			<div class="subTXT">
+				<p>"Friendly, skilled, talented!"</p>
+				<p class="name"><strong>- P.T. Kemper</strong></p>
+			</div>
+		</div>
+		</center>
+	</div>
+
+	<div class="col-3">
+		<center>
+		<div class="cardRev">
+			<div class="cardH">
+				<div class="star"><span class="fa fa-star"></span> <span class="fa fa-star"></span> <span class="fa fa-star"></span> <span class="fa fa-star"></span> <span class="fa fa-star"></span></div>
+			</div>
+			<div class="subTXT">
+				<p>"Fantastic service for a reasonable price. We have found a new barber!"</p>
+				<p class="name"><strong>- Brian Roach</strong></p>
+			</div>
+		</div>
+	</div>
+	</center>
+</div>
+</div>
+<br />
+<!--Services-->
+<div class="row">
+	<div class="col-12">
+    <a name="serv"></a>
+	<section class="paraSer">
+		<div class="paraInner">
+			<h2><span class="header">Services</span></h2>
+		</div>
+	</section>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-12">
+	<div class="tabBH">
+		<h2>Hair Cuts <span class="fa fa-caret-down"></span></h2>
+	</div>
+		<div class="tabTH">
+	<center>
+			<div class="cardL">
+				<div class="cardH">
+					<h3>Hair Cuts</h3>
+				</div>
+				<div class="subTXT">
+ 					<p>Do you need a trim or want a whole new hairstyle? Come in and let me trim your hair to your specifications!</p>
+					<br />
+					<center><p>ADULT HAIRCUT  $15<br/>
+						HAIRCUT AND BEARD TRIM  $20<br/>
+						KIDS 12 & UNDER  $12<br />
+						SHAPE UP  $8<br />
+						BEARD TRIM  $10<br /></p>
+					</center>
+				</div>
+			</div>
+		</center>
+	</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-12">
+		<div class="tabBF">
+			<h2>Facial <span class="fa fa-caret-down"></span></h2>
+		</div>
+		<div class="tabTF">
+		<center>
+			<div class="cardL">
+				<div class="cardH">
+					<h3>Facial</h3>
+				</div>
+				<div class="subTXT">
+					<p>The facial refreshes and cleanses the face & hair to remove sweat, build-up, excess oil and other impurities. This is also a way to treat yourself to an ultimate end of the week treat. </p>
+					<br />
+					<center><p>FACIAL  $10</p></center>
+				</div>
+			</div>
+		</center>
+	</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-12">
+		<div class="tabBT">
+			<h2>Beard Trim & Facial <span class="fa fa-caret-down"></span></h2>
+		</div>
+		<div class="tabTT">
+		<center>
+			<div class="cardL">
+				<div class="cardH">
+					<h3>Beard Trim & Facial</h3>
+				</div>
+				<div class="subTXT">
+					<p>Reshape and redefine the beard and mustache. Included in this service is an essential oil infused hot towel treatment.</p>
+					<br />
+					<p>BEARD TRIM & FACIAL  $15</p>
+				</div>
+			</div>
+		</center>
+	</div>
+</div>
+</div>
+
+<div class="row">
+	<div class="col-12">
+		<div class="tabBS">
+			<h2>Shave <span class="fa fa-caret-down"></span></h2>
+		</div>
+		<div class="tabTS">
+		<center>
+			<div class="cardL">
+				<div class="cardH">
+					<h3>Shave</h3>
+				</div>
+				<div class="subTXT">
+ 					<p>Old-fashioned, single-blade razor shave, with the direction of the hair growth on the face. Included in this service is lather and multiple essential oil treated hot towels.</p>
+					<br />
+					<p>CLASSIC SHAVE  $20</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-12">
+		<div class="tabBI">
+			<h2>Image Gallery <span class="fa fa-caret-down"></span></h2>
+		</div>
+		<div class="tabTI">
+
+<div class="slideshow-container">
+  <div class="mySlides fade">
+    <img src="Images/Man 1.jpg" style="width:100%" alt="client 1">
+  </div>
+
+  <div class="mySlides fade">
+    <img src="Images/Man 3.jpg" style="width:100%" alt="client 2">
+  </div>
+
+  <div class="mySlides fade">
+    <img src="Images/Man 4.jpg" style="width:100%" alt="client 3">
+  </div>
+
+  <div class="mySlides fade">
+    <img src="Images/Man 5.jpg" style="width:100%" alt="client 4">
+  </div>
+
+  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+		<div style="text-align:center">
+  		<span class="dot" onclick="currentSlide(1)"></span>
+  		<span class="dot" onclick="currentSlide(2)"></span>
+  		<span class="dot" onclick="currentSlide(3)"></span>
+  		<span class="dot" onclick="currentSlide(4)"></span>
+		</div>
+
+
+		</div>
+	</div>
+</div>
+<!--Location-->
+<div class="row">
+	<div class="col-12">
+    <a name="loc"></a>
+	<section class="paraLoc">
+		<div class="paraInner">
+			<h2><span class="header">Location</span></h2>
+		</div>
+	</section>
+	</div>
+</div>
+<div class="row">
+	<div class="col-6">
+	<div class="textBoxLoc">
+		<h3 class="title">Google Maps</h3>
+		<div class="mapCon">
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3776.247811903637!2d-84.56163158466977!3d38.20968717968249!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88423f7484e5bfeb%3A0x539df5e465cd8fed!2s112+E+Main+St%2C+Georgetown%2C+KY+40324!5e1!3m2!1sen!2sus!4v1473444186293" frameborder="0" style="border:0" allowfullscreen class="map"></iframe>
+
+		<script src="https://maps.googleapis.com/maps/api/js?callback=myMap"></script>
+		</div>
+	</div>
+	</div>
+	<div class="col-6">
+		<div class="textBoxLoc">
+			<h3 class="title">Street View</h3>
+			<center><img src="Images/streetview.jpg" class="street" alt="Street View"></center>
+		</div>
+	</div>
+</div>
+<!--Contact-->
+<div class="row">
+	<div class="col-12">
+    <a name="con"></a>
+	<section class="paraCon">
+		<div class="paraInner">
+			<h2><span class="header">Contact</span></h2>
+		</div>
+	</section>
+	</div>
+</div>
+<div class="row">
+	<div class="col-6">
+		<center>
+		<div class="card">
+			<div class="cardH">
+				<h3>Hours of Operation</h3>
+			</div>
+			<div class="subTXT">
+				<ul>
+					<li>Monday-Tuesday: 10:00 A.M. - 5:00 P.M.</li>
+					<li>Wednesday: Closed</li>
+					<li>Thursday-Friday: 10:00 A.M. - 6:00 P.M.</li>
+					<li>Saturday: 9:00 A.M. - 3:00 P.M.</li>
+					<li>Sunday: Closed</li>
+				</ul>
+			</div>
+		</div>
+	</center>
+	</div>
+	<div class="col-6">
+		<center>
+		<div class="card">
+			<div class="cardH">
+				<h3>Contact</h3>
+			</div>
+			<div class="subTXT">
+				<ul>
+					<li><span class="fa fa-user"></span> Jerome Graham</li>
+					<li><span class="fa fa-phone"></span> (859)475-4051</li>
+					<li><span class="fa fa-envelope"></span> <span class="emailT">elitebarbershop502@gmail.com</span></li>
+					<li><span class="fa fa-map"></span> 112 E Main St, Suite 204<br /> Georgetown, KY 40324</li>
+				</ul>
+				<br />
+			</div>
+		</div>
+	</center>
+
+	</div>
+</div>
+<div class="row">
+	<div class="col-6">
+		<center>
+		<div class="card">
+			<div class="cardH">
+				<h3>Social Media</h3>
+			</div>
+			<div class="subTXT">
+			<div id="timesSoc">
+				<ul>
+					<li><a href="https://www.facebook.com/Elite-Barber-Shop-1690990447846266/info"><span class="fa fa-facebook-square fa-5x"></span></a></li>
+					<li><a href="https://twitter.com/TheBarberRome?lang=en"><span class="fa fa-twitter-square fa-5x"></span></a></li>
+				</ul>
+			</div>
+			</div>
+		</div>
+		</center>
+	</div>
+	<div class="col-6">
+		<center>
+		<div class="card">
+			<div class="cardH">
+				<h3>Email Jerome!</h3>
+			</div>
+			<div class="subTXT">
+			<form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+				<center><input class="input" type="text" name="name" placeholder="Name"></center>
+				<br />
+				<center><input class="input" type="text" name="email" placeholder="Email"></center>
+				<br />
+				<center><input class="input" type="text" name="sub" placeholder="Subject"></center>
+				<br />
+				<center><textarea class="inputText" name="text" placeholder="Content"></textarea></center>
+				<br />
+				<center><input type="submit" class="button"></center>
+			</form>
+			</div>
+		</div>
+	</center>
+	</div>
+</div>
+
+<br />
+<br />
+
+<!--footer-->
+<div class="footer">
+	<div class="row">
+    	<div class="col-12">
+        <div id="footNav">
+        	<center><ul>
+            	<li><a href="#home">Home</a></li>
+							<li><a href="#serv">Services</a></li>
+							<li><a href="#loc">Location</a></li>
+							<li><a href="#con">Contact</a></li>
+            </ul></center>
+        </div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-12">
+			<h4 class="titleFoot">&copy;<span id="year"></span> FCCTC/Stilio Development</h4>
+        </div>
+		<div class="col-12">
+			<h5 class="titleFootPromo">Promo Code: #StraightRazor</h5>
+    </div>
+</div>
+
+<script src="barber.js"></script>
+<script>
+$(document).ready(function(){
+    $(".tabBH").click(function(){
+        $(".tabTH").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".tabBF").click(function(){
+        $(".tabTF").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".tabBT").click(function(){
+        $(".tabTT").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".tabBS").click(function(){
+        $(".tabTS").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".tabBI").click(function(){
+        $(".tabTi").slideToggle("slow");
+    });
+});
+</script>
+
+<?php
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		$name = test_input($_POST["name"]);
+		$email = test_input($_POST["email"]);
+		$sub = test_input($_POST["sub"]);
+		$text = test_input($_POST["text"]);
+	}
+	function test_input($data) {
+		$data = htmlspecialchars($data);
+		$data = trim($data);
+		$data = stripslashes($data);
+		return $data;
+	}
+
+	if ($sub == "" || $sub == null) {
+		return false;
+	}
+	if ($text == "" || $text == null) {
+		return false;
+	}
+	if ($email == "" || $email == null) {
+		return false;
+	}
+
+	mail("elitebarbershop502@gmail.com", $sub, $text, "From: ". $email);
+?>
+</body>
+</html>
